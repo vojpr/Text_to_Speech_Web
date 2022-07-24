138b24c5e291c87adda8733acc68eef3caea2eb0
vojtech <p.prusavojta@gmail.com>
2022-07-24 20:09:40 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, send_from_directory
+from io import BytesIO
+import pyttsx3
+from PyPDF2 import PdfReader
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    error = None
+    if request.method == 'POST':
+        try:
+            # Deletes previously created mp3 file
+            current_directory = os.getcwd()
+            files_in_dir = os.listdir()
+            for file in files_in_dir:
+                if file == "zpeechy.mp3":
+                    os.remove(file)
+            # Gets string from uploaded PDF
+            pdf_file = request.files['file']
+            bytes_file = BytesIO(pdf_file.read())
+            pdf_string = ""
+            reader = PdfReader(bytes_file)
+            number_of_pages = len(reader.pages)
+            for each in range(number_of_pages):
+                page = reader.pages[each]
+                pdf_string += page.extract_text()
+            # Creates mp3 file from string
+            engine = pyttsx3.init()
+            engine.setProperty('rate', 155)
+            engine.save_to_file(pdf_string, "zpeechy.mp3")
+            engine.runAndWait()
+            return send_from_directory(current_directory, "zpeechy.mp3", as_attachment=True)
+        except:
+            error = "Upload your PDF file first"
+            return render_template("index.html", error=error)
+    else:
+        return render_template("index.html", error=error)
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/js/main.js
@@ -0,0 +1,94 @@
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
+
+})()
